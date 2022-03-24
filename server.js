c5e13fb3cf697433d1d7b1c531840402a07ed5a3
Ben Guarasci <benguarasci@icloud.com>
2022-03-23 22:57:38 -0700

    test this

--- server.js
+++ server.js
@@ -7,7 +7,9 @@ const redisClient = require('./redis-client');
 app.get('/store/:key', async (req, res) => {
   const { key } = req.params;
   const value = req.query;
-  await redisClient.setAsync(key, JSON.stringify(value));
+  const date = new Date();
+  const buy = {...value, date: date.toLocaleDateString("en-US")};
+  await redisClient.setAsync(key, JSON.stringify(buy));
   return res.send('Success');
 });
 
@@ -18,7 +20,7 @@ app.get('/:key', async (req, res) => {
 });
 
 app.get('/', (req, res) => {
-  return res.send('Hello world');
+    res.sendFile(path.join(__dirname, '/index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
